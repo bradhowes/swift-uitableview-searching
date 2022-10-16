34780008999d093f414135d60a3d6d675bced8cc
Brad Howes <bradhowes@mac.com>
2022-10-16 10:37:15 +0200

    cleanup

--- Searching/ArrayTransitionStep.swift
+++ Searching/ArrayTransitionStep.swift
@@ -3,24 +3,24 @@ import Foundation
 // Based on https://lukaszielinski.de/blog/posts/2019/02/03/calculating-the-deltas-for-performbatchupdates/
 
 enum ArrayTransitionStep {
-
+  
   case added(Int)
   case deleted(Int)
   case moved(Int, Int)
-
+  
   /**
    Calculate the number of steps necessary to change from one collection of integers to another collection.
-
+   
    - parameter from: the starting collection state
    - parameter to: the final collection state
    - returns: the steps
    */
   static func changes(from: [Int], to: [Int]) -> [ArrayTransitionStep] {
-
+    
     // Generate mappings from values to array indices
     let fromMap = [Int: Int](uniqueKeysWithValues: from.enumerated().map { ($0.1, $0.0) } )
     var toMap = [Int: Int](uniqueKeysWithValues: to.enumerated().map { ($0.1, $0.0) } )
-
+    
     var actions = [ArrayTransitionStep]()
     for value in fromMap.keys.sorted() {
       let fromIndex = fromMap[value]!
@@ -33,7 +33,7 @@ enum ArrayTransitionStep {
         actions.append(.deleted(fromIndex))
       }
     }
-
+    
     return actions + toMap.keys.map { .added(toMap[$0]!) }
   }
 }
