3d4ebe990554718f973db8b591009e0dc94ecbe7
Brad Howes <bradhowes@mac.com>
2023-08-15 16:45:26 +0200

    Acquire functionality from ViewController

--- Searching/DataSource.swift
+++ Searching/DataSource.swift
@@ -6,6 +6,23 @@ import UIKit
 class DataSource : UITableViewDiffableDataSource<Section, Title> {
 
   /**
+   Create a new snapshot to be used in a DataSource.
+
+   - parameter term: the search term to use to filter titles to be in the new collection (empty string shows all)
+   - returns: new NSDiffableDataSourceSnapshot instance
+   */
+  static func buildSnapshot(term: String) -> NSDiffableDataSourceSnapshot<Section, Title> {
+    let titles = term.isEmpty ? allTitles : (allTitles.filter { $0.matches(term.uppercased()) })
+    let tableSections = partitionTitles(titles)
+
+    var snapshot = NSDiffableDataSourceSnapshot<Section, Title>()
+    snapshot.appendSections(tableSections.map { $0.section })
+    tableSections.forEach { snapshot.appendItems($0.titles, toSection: $0.section) }
+
+    return snapshot
+  }
+
+  /**
    Obtain a section title.
 
    - parameter tableView: the table view to show
